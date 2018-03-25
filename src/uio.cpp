e0c5b83d2c19c8435fb94e26f663745f4df35a75
Simone Basso <bassosimone@gmail.com>
2018-03-25 14:56:48 +0200

    uio.cpp: avoid fixed size buffers

--- src/uio.cpp
+++ src/uio.cpp
@@ -1,6 +1,10 @@
 #include <remk/platform/uio.h>
 
+#ifdef _WIN32
 #include <assert.h>
+#include <limits.h>
+#include <memory>
+#endif
 
 #include <remk/platform/context.h>
 
@@ -14,27 +18,30 @@ remk_platform_ssize_t remk_platform_writev(
     }
     // To simplify reasoning about the code below in both Win32 and Win64, we
     // fail when requested to send more than `INT_MAX - 1` bytes.
-    constexpr auto max_size = INT_MAX - 1;
-    WSABUF buffs[IOV_MAX];
-    remk_platform_size_t total = 0;
-    for (int i = 0; i < iovcnt; ++i) {
-        auto iop = &iov[i];
-        if (iop->iov_len > max_size || total > max_size - iop->iov_len) {
-            WSASetLastError(WSAEINVAL);
-            return -1;
+    constexpr SIZE_T max_size = INT_MAX - 1;
+    auto buffs = std::make_unique<WSABUF[]>(iovcnt);
+    {
+        auto iop = &iov[0];
+        auto bufp = buffs.get();
+        SIZE_T total = 0;
+        for (auto i = 0; i < iovcnt; ++i, ++iop, ++bufp) {
+            if (iop->iov_len > max_size || total > max_size - iop->iov_len) {
+                WSASetLastError(WSAEINVAL);
+                return -1;
+            }
+            total += iop->iov_len;
+            bufp->len = (unsigned long)iop->iov_len;
+            bufp->buf = (char *)iop->iov_base;
         }
-        total += iop->iov_len;
-        buffs[i].len = (unsigned long)iop->iov_len;
-        buffs[i].buf = (char *)iop->iov_base;
+        assert(total <= max_size);
     }
-    assert(total <= max_size);
     DWORD nsent = 0;
-    if (ctx->system_wsasend(socket, buffs, (DWORD)iovcnt, &nsent, 0, nullptr,
-              nullptr) != 0) {
+    if (ctx->system_wsasend(socket, buffs.get(), (DWORD)iovcnt, &nsent, 0,
+              nullptr, nullptr) != 0) {
         return -1;
     }
     assert(nsent <= max_size);
-    return (remk_platform_ssize_t)nsent;
+    return nsent;
 #else
     return ctx->system_writev(socket, iov, iovcnt);
 #endif
