4fcb73dabe7168a72477ba08f4f527bd5fb8baf5
Simone Basso <bassosimone@gmail.com>
2018-03-28 23:08:40 +0200

    Finish simplifying the tree

--- include/remk/platform.h
+++ include/remk/platform.h
@@ -1,7 +1,6 @@
 #ifndef REMK_PLATFORM_H
 #define REMK_PLATFORM_H
 
-#include <remk/platform/aaa_base.h>
 #include <remk/platform/context.h>
 
 #endif
--- include/remk/platform/context.h
+++ include/remk/platform/context.h
@@ -2,10 +2,37 @@
 #define REMK_PLATFORM_CONTEXT_H
 #ifdef __cplusplus
 
-#include <remk/platform/aaa_base.h>
+#ifdef _WIN32
+#include <basetsd.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
+#include <errno.h>
+#include <netdb.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#endif
+
+#include <time.h>
 
 #include <sstream>
 #include <string>
+
+#ifndef TIME_UTC
+#define TIME_UTC 1
+#endif
+
+#ifdef _WIN32
+struct iovec {
+    void *iov_base;
+    SIZE_T iov_len;
+};
+#define IOV_MAX 64
+#endif
 
 #define REMK_PLATFORM_LOG_WARNING 0
 #define REMK_PLATFORM_LOG_INFO 1
